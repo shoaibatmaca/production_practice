a2c8eca87452590564f648173fcd9141ddbea84b
M. Shoaib <mshoaibkaglur@gmail.com>
2025-08-29 01:00:46 +0500

    update code

--- src/core/settings.py
+++ src/core/settings.py
@@ -9,25 +9,44 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
+from dotenv import load_dotenv
+load_dotenv()
+
 import os 
 from pathlib import Path
+from decouple import config
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("Debu") == "True"
+
+DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "0"  # "1" = True, "0" = False
 
 
-ALLOWED_HOSTS = []
 
+
+ALLOWED_HOSTS = ["www.hungry.com",
+                 ".railway.app"]
+
+CSRF_TRUSTED_ORIGINS=[
+    "https://*.railway.app"
+]
+if DEBUG:
+    ALLOWED_HOSTS=['*']
+    
 
 # Application definition
 
